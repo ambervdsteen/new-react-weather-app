8b9b17f13906f66808e285c86a1171e57fc5884a
ambervdsteen <155630578+ambervdsteen@users.noreply.github.com>
2024-08-21 11:55:21 +0200

    created structure base for weather app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="Weather App">
+      <h2>Welcome to the weather app</h2>
+      <h3>check out the weather in your city</h3>
+        <footer>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        >        
+        </a>     </footer>
       </header>
     </div>
   );
