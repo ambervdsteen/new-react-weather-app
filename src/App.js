682736a99d3a9e7cfd39b38d272d7eee6adf4b9a
ambervdsteen <155630578+ambervdsteen@users.noreply.github.com>
2024-08-21 11:58:03 +0200

    added footer with github link

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
       <header className="Weather App">
       <h2>Welcome to the weather app</h2>
       <h3>check out the weather in your city</h3>
-        <footer>
+        <footer> this page was coded by 
         <a
           className="App-link"
-          href="/"
+          href="https://github.com/ambervdsteen/new-react-weather-app"
           target="_blank"
           rel="noopener noreferrer"
-        >        
-        </a>     </footer>
+        > Amber van der Steen      
+        </a>  </footer>
       </header>
     </div>
   );
