3d69db49e20e2b08270575262c6c7a12e429101b
ambervdsteen <155630578+ambervdsteen@users.noreply.github.com>
2024-08-22 22:02:35 +0200

    fixed minute and hour formatting

--- src/DateFormat.js
+++ src/DateFormat.js
@@ -7,15 +7,14 @@ let datenumber=date.getDate()
 let months=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 let month=months[date.getMonth()]
 let hour = date.getHours()
-if (hour<10) {hour=`0 ${hour}`;}
+if (hour<10) {hour=`0${hour}`;}
 let minutes=date.getMinutes()
-if (minutes < 10) {minutes =`0 ${minutes}`;}
+if (minutes < 10) {minutes =`0${minutes}`;}
 
 return(
    
    <div>
-    
-{day}, {month} {datenumber} {hour}:{minutes}</div>
+{day}, {month} {datenumber}, {hour}:{minutes}</div>
 
 
 );
