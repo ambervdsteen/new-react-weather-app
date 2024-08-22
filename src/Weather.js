da3f61423cfdd2017398b272f3227c1c6c5a07aa
ambervdsteen <155630578+ambervdsteen@users.noreply.github.com>
2024-08-22 10:54:53 +0200

    added more css styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,31 +4,33 @@ import './Weather.css';
 function Weather(){
     return (
        <div className="Weather">
+
       
-<form>
+      
+<form className="mb-3">
 <div className="row">
     <div className="col-9">
-    <input type="search" placeholder="search for your city"/></div>
+    <input type="search" placeholder="search for your city" className="searchBar w-100" autoFocus="on"/></div>
     <div className="col-3">
-    <input type="submit" className="btn btn-dark"/></div>
+    <input type="submit" className="btn btn-dark w-100"/></div>
     </div>
 </form>
         <div className="row">
-<div className="col-6">
+<div className="col-9">
 <ul>
     <li>Date Thursday 22nd of August, 2024</li>
-    <li>DescriptionSunny with scattered clouds</li>
+    <li>Description Sunny with scattered clouds</li>
 </ul>
 </div>
-<div className="col-6">
+<div className="col-3">
 <ul>
-    <li>precipitation</li>
-    <li>humidity</li>
-    <li>wind</li>
+    <li>15% precipitation</li>
+    <li>65% humidity</li>
+    <li>10 km/h wind</li>
 </ul>
 </div>
 
-<div className="YourCity">
+<div className="YourCity mt-3" >
     
         <h1>City BARCELONA</h1>
         <h2>Current Temperature 26°C</h2>
@@ -37,7 +39,7 @@ function Weather(){
 </div>
 
         </div>
-
+     
        </div>
     )
 }
